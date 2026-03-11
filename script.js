49ca68c353830e4511327a5bb35829bfa8d7b832
sayeed <expsayeedshorif@gmail.com>
2026-03-11 19:38:33 +0600

    added home / hero section

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const humburgerMenu = document.querySelector('#hamburger')
 const openMenu = document.querySelector('#open')
 const closeMenu = document.querySelector('#close')
+const linkWrapper = document.querySelector('#linkWrapper')
+const navItems = document.querySelector('#navItems')
 function handleHambugerClick() {
     humburgerMenu.addEventListener('click', () => {
         openMenu.classList.toggle('hidden')
         closeMenu.classList.toggle('hidden')
+        navItems.classList.toggle('hamburgerOff')
     })
+    navItems.querySelectorAll('a').forEach(item => {
+        item.addEventListener('click', () => {
+            navItems.classList.remove('hamburgerOff')
+        })
+    })
+
 }
 
 handleHambugerClick()
